bf397431c9b3cea595cd1d971bd5fa9ae1337893
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-21 13:42:18 -0800

    Pagination complete

--- app.js
+++ app.js
@@ -1,182 +1,74 @@
-const data = [
-  {
-    id: 1,
-    name: "Cameron Williamson",
-    email: "cameron.wiliamson@example.com",
-    title: "Software Developer",
-  },
-  {
-    id: 2,
-    name: "Jenny Wilson",
-    email: "jenny.wilson@example.com",
-    title: "Project Manager",
-  },
-  {
-    id: 3,
-    name: "Jane Cooper",
-    email: "jane.cooper@example.com",
-    title: "Marketing Coordinator",
-  },
-  {
-    id: 4,
-    name: "Wade Warren",
-    email: "wade.warren@example.com",
-    title: "Software Tester",
-  },
-  {
-    id: 5,
-    name: "Esther Howard",
-    email: "esther.howard@example.com",
-    title: "Web Designer",
-  },
-  {
-    id: 6,
-    name: "Kristin Watson",
-    email: "kristin.watson@example.com",
-    title: "Marketing Coordinator",
-  },
-  {
-    id: 7,
-    name: "Esther Howard",
-    email: "esther.howard@example.com",
-    title: "Web Designer",
-  },
-  {
-    id: 8,
-    name: "Leslie Alexander",
-    email: "leslie.alexander@example.com",
-    title: "UI/UX Designer",
-  },
-  {
-    id: 9,
-    name: "Ralph Edwards",
-    email: "ralph.edwards@example.com",
-    title: "Software Tester",
-  },
-  {
-    id: 10,
-    name: "Courtney Henry",
-    email: "courtney.henry@example.com",
-    title: "Ethical Hacker",
-  },
-  {
-    id: 11,
-    name: "Willie Jennings",
-    email: "willie.jennings@example.com",
-    title: "Team Leader",
-  },
-  {
-    id: 12,
-    name: "Neveah Simmons",
-    email: "neveah.simmons@example.com",
-    title: "Team Leader",
-  },
-  {
-    id: 13,
-    name: "Theresa Webb",
-    email: "theresa.webb@example.com",
-    title: "Software Tester",
-  },
-  {
-    id: 14,
-    name: "Debbe Baker",
-    email: "debbe.baker@example.com",
-    title: "Software Developer",
-  },
-  {
-    id: 15,
-    name: "Ronald Richards",
-    email: "ronald.richards@example.com",
-    title: "Software Developer",
-  },
-  {
-    id: 16,
-    name: "Deanna Curtis",
-    email: "deanna.curtis@example.com",
-    title: "Scrum Master",
-  },
-  {
-    id: 17,
-    name: "Felicia Reid",
-    email: "felicia.reed@example.com",
-    title: "Ethical Hacker",
-  },
-  {
-    id: 18,
-    name: "Jessie Alexander",
-    email: "jessie.alexander@example.com",
-    title: "Project Manager",
-  },
-  {
-    id: 19,
-    name: "Sam Smith",
-    email: "sam.smith@example.com",
-    title: "Ethical Hacker",
-  },
-  {
-    id: 20,
-    name: "Eleanor Rigby",
-    email: "eleanor.rigby@example.com",
-    title: "UI/UX Designer",
-  },
-  {
-    id: 21,
-    name: "Devon Lane",
-    email: "devon.lane@example.com",
-    title: "Team Leader",
-  },
-  {
-    id: 22,
-    name: "Guy Hawkins",
-    email: "guy.hawkins@example.com",
-    title: "Team Leader",
-  },
-  {
-    id: 23,
-    name: "Jim Parks",
-    email: "jim.parks@example.com",
-    title: "Ethical Hacker",
-  },
-  {
-    id: 24,
-    name: "Susanne Ford",
-    email: "susanne.ford@example.com",
-    title: "Software Developer Manager",
-  },
-  {
-    id: 25,
-    name: "Kathryn Murphy",
-    email: "kathryn.murphy@example.com",
-    title: "Project Manager",
-  },
-  {
-    id: 26,
-    name: "Cody Fisher",
-    email: "cody.fisher@example.com",
-    title: "Software Developer",
-  },
-  {
-    id: 27,
-    name: "Jane Cooper",
-    email: "jane.cooper@example.com",
-    title: "Software Tester",
-  },
-  {
-    id: 28,
-    name: "Karen MacAfee",
-    email: "karen.macafee@example.com",
-    title: "UI/UX Designer",
-  },
-  {
-    id: 29,
-    name: "Dianne Russell",
-    email: "dianne.russell@example.com",
-    title: "Ethical Hacker",
-  },
-  {
-    id: 30,
-    name: "Bessie Cooper",
-    email: "bessie.cooper@example.com",
-    title: "Scrum Master",
-  },
-];
+import { data } from "./data.js";
+
+(function init() {
+  renderTableRows();
+  addEventListeners();
+})();
+
+function addEventListeners() {
+  const pagination = document.querySelector(".pagination");
+  const buttons = pagination.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", handlePaginationClick);
+  });
+}
+
+function renderTableRows(start = 0, end = 10) {
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = "";
+
+  data.slice(start, end).forEach((item) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td class="id">${item.id}</td>
+        <td class="name">
+            <input type="text" disabled name="person-name-${item.id}" value="${item.name}" />
+        </td>
+        <td class="email">
+            <input type="email" disabled name="person-email-${item.id}" value="${item.email}" />
+        </td>
+        <td class="title">
+            <input type="text" disabled name="person-title-${item.id}" value="${item.title}" />
+        </td>
+        <td class="actions">
+            <button class="update" name="person-update-${item.id} id="personUpdate${item.id}">
+            <img src="./images/update.svg" alt="Update" class="update" />
+            </button>
+            <button class="edit" name="person-edit-${item.id}" id="personEdit${item.id}">
+            <img src="./images/edit.svg" alt="Edit" class="edit" />
+            </button>
+        </td>
+    `;
+    tableBody.appendChild(row);
+  });
+}
+
+function handlePaginationClick(event) {
+  const pageNumberInput = document.querySelector("#currentPage");
+  const totalPages = document.querySelector("#totalPages");
+  const pageNumber = Number(pageNumberInput.value);
+  const maxPageNumber = Math.ceil(data.length / 10);
+
+  totalPages.textContent = maxPageNumber;
+
+  if (event.target.parentElement.id === "next") {
+    if (pageNumber < maxPageNumber) {
+      pageNumberInput.value = pageNumber + 1;
+      renderTableRows(pageNumber * 10, (pageNumber + 1) * 10);
+    } else {
+      pageNumberInput.value = 1; // Go back to the first page
+      renderTableRows(0, 10);
+    }
+  }
+
+  if (event.target.parentElement.id === "previous") {
+    if (pageNumber > 1) {
+      pageNumberInput.value = pageNumber - 1;
+      renderTableRows((pageNumber - 2) * 10, (pageNumber - 1) * 10);
+    } else {
+      pageNumberInput.value = maxPageNumber; // Go to the last page
+      renderTableRows((maxPageNumber - 1) * 10, maxPageNumber * 10);
+    }
+  }
+}
